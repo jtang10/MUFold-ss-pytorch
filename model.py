e7fb50bd309e4a02ae9bbae27165233185168d68
JingningTang <tangjingning@gmail.com>
2017-10-06 14:33:25 -0500

    add argparse

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ from __future__ import print_function, division
 import time
 import argparse
 import os
+import shutil
 
 import torch
 import torch.nn as nn
@@ -15,10 +16,11 @@ from data_loading import Protein_Dataset
 
 parser = argparse.ArgumentParser(description='PyTorch implementation of Mufold-ss paper')
 parser.add_argument('run', metavar='DIR', help='directory to save the summary and model')
-parser.add_argument('--epochs', default=10, type=int, metavar='N', help='number of epochs to run')
-parser.add_argument('-b', '--batch_size', default=64, type=int, metavar='N', help='batch size of training data')
-parser.add_argument('-lr', '--learning_rate', default=1e-4, type=float, metavar='LR', help='initial learning rate')
-parser.add_argument('--max_seq_len', default=300, type=int, metavar='N', help='cutoff sequence length of training data')
+parser.add_argument('--epochs', default=10, type=int, metavar='N', help='number of epochs to run (default: 10)')
+parser.add_argument('-b', '--batch_size', default=64, type=int, metavar='N', help='batch size of training data (default: 64)')
+parser.add_argument('-lr', '--learning_rate', default=1e-4, type=float, metavar='LR', help='initial learning rate (default: 1e-4)')
+parser.add_argument('--max_seq_len', default=300, type=int, metavar='N', help='cutoff sequence length of training data (default: 300)')
+parser.add_argument('-c', '--clean', action='store_true', default=False, help="If true, clear the summary directory first")
 args = parser.parse_args()
 
 use_cuda = torch.cuda.is_available()
@@ -124,7 +126,11 @@ def evaluate(cnn, dataloader):
     return correct / total
 
 def train():
-    writer = SummaryWriter(log_dir=os.path.join("logger", args.run))
+    writer_path = os.path.join("logger", args.run)
+    if os.path.exists(writer_path) and args.clean:
+        shutil.rmtree(writer_path)
+
+    writer = SummaryWriter(log_dir=writer_path)
     cnn = MUFold_ss()
     if use_cuda:
         start = time.time()
@@ -132,20 +138,16 @@ def train():
         print("Spent {:.2f}s to load GPU model".format(time.time() - start))
 
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(cnn.parameters(), lr=learning_rate)
+    optimizer = torch.optim.Adam(cnn.parameters(), lr=args.learning_rate)
 
     start = time.time()
-    for epoch in range(epochs):
+    for epoch in range(args.epochs):
         cnn.train()
-        print("Epoch {} out of {}".format(epoch + 1, epochs))
+        print("Epoch {} out of {}".format(epoch + 1, args.epochs))
         for i, (features, labels) in enumerate(train_loader):
             features = cuda_var_wrapper(features)
             labels = cuda_var_wrapper(labels)
             output = cnn(features)
-            # print(output.size()[1])
-            # print(type(output.size()))
-            # print(type(output.data))
-            # break
             optimizer.zero_grad()
             loss = criterion(output.view(-1, 8), labels.view(-1))
             writer.add_scalar('data/loss', loss.data[0], i + epoch * len(train_dataset)//args.batch_size)
@@ -153,12 +155,13 @@ def train():
             optimizer.step()
             if (i+1) % 10 == 0:
                 print ('Epoch [%d/%d], Iter [%d/%d] Loss: %.4f' 
-                   %(epoch+1, epochs, i+1, len(train_dataset)//args.batch_size, loss.data[0]))
+                   %(epoch+1, args.epochs, i+1, len(train_dataset)//args.batch_size, loss.data[0]))
 
         accuracy = evaluate(cnn, valid_loader)
         writer.add_scalar('data/accuracy', accuracy, epoch * len(train_dataset)//args.batch_size)
         print("Validation accuracy {:.3f}".format(accuracy))
-    print("Time spent: {:.2f}s".format(time.time() - start))
+
+    print("Time spent on training: {:.2f}s".format(time.time() - start))
     writer.export_scalars_to_json("./all_scalars.json")
     writer.close()
 
@@ -170,9 +173,14 @@ def train():
     save_model_dir = os.path.join(os.getcwd(), "saved_model")
     if not os.path.exists(save_model_dir):
         os.makedirs(save_model_dir)
-    model_counter = 0
-    model_name = ['cnn', str(model_counter), 'epochs', str(args.epochs), ]
-    torch.save(the_model.state_dict(), )
+    model_name = ['cnn', 'epochs', str(args.epochs),
+                  'batch', str(args.batch_size),
+                  'lr', str(args.learning_rate),
+                  'max_seq_len', str(args.max_seq_len)]
+    model_name = '_'.join(model_name)
+    model_path = os.path.join(save_model_dir, model_name)
+    if not os.path.exists(model_path):
+        torch.save(cnn.state_dict(), model_path)
 
 if __name__ == '__main__':
     train()
